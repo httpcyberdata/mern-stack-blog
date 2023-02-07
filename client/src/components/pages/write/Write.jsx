9414cc37303fcb30543a7a95658d2b7004c9588a
httpcyberdata <shieldedsafety@gmail.com>
2023-02-07 12:55:55 -0500

    added textarea story tell box

--- client/src/components/pages/write/Write.jsx
+++ client/src/components/pages/write/Write.jsx
@@ -11,6 +11,12 @@ export default function Write() {
                 <input type="file" id="fileInput" style={{ display: "none" }} /> 
                 <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
             </div>
+            <div className="writeFormGroup">
+                <textarea placeholder='Tell your story...' type="text" className="writeInput writeText">
+
+                </textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
         </form>
     </div>
   )
