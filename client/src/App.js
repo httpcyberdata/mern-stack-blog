e97e83256c87b4943b1fa891d1012047e631f94c
httpcyberdata <shieldedsafety@gmail.com>
2023-02-08 14:33:05 -0500

    created register component & matching .css file

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import Home from './components/pages/home/Home';
 import Single from './components/pages/single/Single';
 import Write from './components/pages/write/Write';
 import Settings from './components/pages/settings/Settings';
+import Login from './components/pages/login/Login'
 function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Login />
     </>
   );
 }
