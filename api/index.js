13bc6af96bd41b855af6df3d7047e0c13eb457f9
httpcyberdata <shieldedsafety@gmail.com>
2023-02-09 14:53:59 -0500

    created api routes and models folders, setted up mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-console.log("hello");
+dotenv.config()
 
-app.listen("6000", () => {
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log('Connected to Mongo DB')).catch(err => console.log(err))
+
+app.use("/lama", (req, res) => {
+    console.log("hey this is main url");
+})
+
+app.listen("4000", () => {
     console.log('Backend is running.')
 })
