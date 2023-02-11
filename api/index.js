0b79ed48181ba44f61709cd9e530cc4c20f745a3
httpcyberdata <shieldedsafety@gmail.com>
2023-02-11 13:32:46 -0500

    register user route implemented and express.json middleware

--- api/index.js
+++ api/index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
 dotenv.config()
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(console.log('Connected to Mongo DB')).catch(err => console.log(err))
 
-app.use("/lama", (req, res) => {
-    console.log("hey this is main url");
-})
+app.use('/api/auth', authRoute)
 
 app.listen("4000", () => {
     console.log('Backend is running.')
