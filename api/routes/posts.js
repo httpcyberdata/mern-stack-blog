c8f98afebac370f05b05b856c76dcd5879bbb032
httpcyberdata <shieldedsafety@gmail.com>
2023-02-12 16:01:18 -0500

    chore: Add Delete post functionality

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -14,34 +14,50 @@ router.post("/", async (req, res) => {
     }
 });
 
-
-
 // Update posts
-router.delete("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id) {
-        try {
-            const user = await User.findById(req.params.id);
-        try {
-            await Post.deleteMany({ username: user.username})
-            await User.findByIdAndDelete(req.params.id)
-            res.status(200).json("User has been deleted...")
-        } catch (err) { 
-            res.status(500).json(err);
-        }
-    } catch (err) {
-        res.status(404).json("User not found!")
-    }
-    } else {
-        res.status(401).json("You can delete only your account!")
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                    }, { new: true });
+                    res.status(200).json(updatedPost)
+                } catch(err) {
+                    res.status(500).json(err);
+                }
+            } else {
+                res.status(401).json("You can update only your post!");
+            }
+    } catch(err) {
+        res.status(500).json(err);
     }
 });
 
 // Delete post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.username === req.body.username) {
+            try {
+                    await post.delete()
+                    res.status(200).json("Post has been deleted...");
+                } catch(err) {
+                    res.status(500).json(err);
+                }
+            } else {
+                res.status(401).json("You can delete only your post!");
+            }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 
 // Get post
 router.get("/:id", async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
+        const post = await User.findById(req.params.id);
         const { password, ...others} = user._doc;
         res.status(200).json(others);
     } catch(err) {
