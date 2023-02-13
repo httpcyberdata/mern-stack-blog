f59f3080423a310d5b0cad1d56e52502a322c89b
httpcyberdata <shieldedsafety@gmail.com>
2023-02-13 17:46:46 -0500

    chore: Add GET post by id

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -54,12 +54,11 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
-// Get post
+// Get post - not done
 router.get("/:id", async (req, res) => {
     try {
-        const post = await User.findById(req.params.id);
-        const { password, ...others} = user._doc;
-        res.status(200).json(others);
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
     } catch(err) {
         res.status(500).json(err);
     }
