70d7270b67205f212d2774b0e3ee9de215903ed7
httpcyberdata <shieldedsafety@gmail.com>
2023-02-13 20:18:36 -0500

    chore: Add file uploadroute to API

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+const multer = require('multer');
+
 dotenv.config()
 app.use(express.json());
 
@@ -13,9 +16,24 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 }).then(console.log('Connected to Mongo DB')).catch(err => console.log(err))
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images") 
+    }, 
+    filename: (req, file, cb) => {
+        cb(null, "hello.jpeg");
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute)
 
 app.listen("4000", () => {
     console.log('Backend is running.')
