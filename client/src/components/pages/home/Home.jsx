32ec2588750e24dc1e5eeb7de46cb8c93a69bf3f
httpcyberdata <shieldedsafety@gmail.com>
2023-02-15 18:08:34 -0500

    chore: Make username clickable and routes to page of articles by the username

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -4,18 +4,20 @@ import Posts from '../../posts/Posts';
 import Sidebar from '../../sidebar/Sidebar';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import {  useLocation } from 'react-router'
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get('/posts');
+      const res = await axios.get('/posts' + search);
       setPosts(res.data)
     }
     fetchPosts()
-  }, [])
+  }, [search])
   return (
-    <>
+    <> 
       <Header />
       <div className="home">
           <Posts posts={posts} />
