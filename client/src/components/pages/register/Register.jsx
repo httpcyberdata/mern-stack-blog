ebec04bbdd3c8312e88f0fcc98ac69b990838954
httpcyberdata <shieldedsafety@gmail.com>
2023-02-16 20:05:12 -0500

    chore: created context state file

--- client/src/components/pages/register/Register.jsx
+++ client/src/components/pages/register/Register.jsx
@@ -53,7 +53,7 @@ export default function Register() {
                 Login
             </Link>
         </button>
-       { error && (  <span>Something went wrong!</span> )}
+       { error && (  <span style={{ color: "red", marginTop: "10px"}}>Something went wrong!</span> )}
     </div>
   )
 }
